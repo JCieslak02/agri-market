aeb8bf8dfffb64e9c9f6a44f4a3926a8c631bf5d
JCieslak <cieslak1510@gmail.com>
2023-02-03 17:03:37 +0100

    initial commit

---
+++ src/main/java/com/jcieslak/farmersmarket/FarmersMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jcieslak.farmersmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmersMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmersMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcieslak/farmersmarket/FarmersMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcieslak.farmersmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmersMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
